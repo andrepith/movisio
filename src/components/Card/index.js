4eb19cc8eb37f867a43193ada5113175f7f66352
Andre <andrepith@yahoo.co.uk>
2020-09-02 16:55:04 +0700

    Create modal

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import {Modal, Row, Col} from 'react-bootstrap'
 
 const Card = ({
   Title,
@@ -7,7 +8,13 @@ const Card = ({
   imdbID,
   history
 }) => {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false);
+  const handleShow = async search => {
+    setShow(true);
+  };
   return (
+    <>
     <div className="card p-4 mb-4" style={{ 
       border: 'solid 1px black'
    }} key={key}>
@@ -22,8 +29,28 @@ const Card = ({
       <div className="card-body text-center">
         <div>{Title}</div>
         <button onClick={() => history.push(`/${imdbID}`)}>Go to detail</button>
+        <button onClick={handleShow}>See Poster</button>
       </div>
     </div>
+    <Modal
+        show={show}
+        onHide={handleClose}
+        dialogClassName="modal__container"
+      >
+        <>
+      <Modal.Header closeButton>
+        <Modal.Title>{Title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Row className="modal__wrapper">
+          <Col sm={12} className="poster text-center">
+            <img src={Poster} alt={Title} style={{ maxHeight: "320px" }} />
+          </Col>
+        </Row>
+      </Modal.Body>
+    </>
+      </Modal>
+    </>
   );
 };
 
