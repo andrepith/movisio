0b0d10262ac617ce2d030cb1c660f60f9aa791a4
Andre <andrepith@yahoo.co.uk>
2020-09-02 16:00:17 +0700

    Adjust dispatch for pagination

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,26 @@
-import React from "react";
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux'
 import Search from 'components/Search'
 import Card from 'components/Card'
+import { getMovieListByTitle } from "store/actions";
 
 const Dashboard = () => {
+  const [page, setPage] = useState(1)
+  const [data, setData] = useState([])
+  const disptach = useDispatch()
   const { movieList } = useSelector(state => state)
+  const fetchApi = async () => {
+    await disptach(getMovieListByTitle({ search: 'Avengers', page }))
+    setData(movieList.Search)
+  }
+
+  useEffect(() => {
+    fetchApi()
+  }, [page])
+
   return (
     <div>
-      <Search />
+      {/* <Search /> */}
       {(movieList.Search || []).map((item, index) => (
         <Card key={index} {...item} />
       ))}
--- src/store/actions/omdbActions/index.js
+++ src/store/actions/omdbActions/index.js
@@ -1,8 +1,8 @@
 import { GET_MOVIE_LIST_BY_TITLE, GET_MOVIE_DETAIL } from "store/types";
 import { omdbService } from "services/omdbServices";
 
-export const getMovieListByTitle = ({ search }) => async dispatch => {
-  const payload = await omdbService({ search: { s: search } });
+export const getMovieListByTitle = ({ search, page }) => async dispatch => {
+  const payload = await omdbService({ search: { s: search, page } });
 
   dispatch({
     type: GET_MOVIE_LIST_BY_TITLE,
